5f27a76e9a7ccb35220d1f6543bee14917dd5170
arthurnzrnk <75807461+arthurnzrnk@users.noreply.github.com>
2025-08-18 16:50:39 -0400

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Dashboard from './pages/Dashboard'
-import Status from './pages/Status'
 import ApiKeys from './pages/ApiKeys'
 import Learning from './pages/Learning'
 import './styles.css'
@@ -14,7 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <App>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/status" element={<Status />} />
+          {/* Status route removed: logs are integrated into Dashboard */}
           <Route path="/apikeys" element={<ApiKeys />} />
           <Route path="/learning" element={<Learning />} />
         </Routes>
